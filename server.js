6cc3a2d05cef4729b5a02e3ed4c9278b127817d4
Torrence Cole <trcole@gmail.com>
2021-04-08 08:39:59 -0400

    auth route to signup user and signed jwt

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes
+app.use('/api/v1/users', require('./routes/api/users'));
+app.use('/api/v1/auth', require('./routes/api/auth'));
+app.use('/api/v1/profile', require('./routes/api/profile'));
+app.use('/api/v1/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server starte on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
